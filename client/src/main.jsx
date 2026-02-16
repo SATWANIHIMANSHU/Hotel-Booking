e4886ee1055e3579778e16a7967b7ba399388c17
Practice <satwanihimanshu35@gmail.com>
2026-02-16 21:07:46 +0530

    fix casing issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { AppProvider } from './context/Appcontext.jsx'
+import { AppProvider } from './context/AppContext.jsx'
 
 
 // Import your Publishable Key
